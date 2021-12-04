f744e0065c2a4e685e6d31ff2caa82ab5c36f787
Denis Sleptsov <dinis68@mail.ru>
2021-12-04 22:16:00 +0300

    1

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('input');
+const btn = document.querySelector('btn');
+const list = document.querySelector('list');
+
+btn.addEventListener('click', function(event)
+{
+    let li = document.createElement('li');
+    li.innerHTML = input.value;
+    
+    list.appendChild(li);
+
+    if(li.innerHTML > 10){
+        li.className = "red";
+    }
+
+    if(li.innerHTML < 10){
+        li.className = "green";
+    }
+
+    if(li.innerHTML == 10){
+        li.className = "blue";
+    }
+
+    if(li.innerHTML)
+
+
+    
+})
+
+
